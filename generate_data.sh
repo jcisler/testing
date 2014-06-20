0115341608cba81a78a08ffd86095a30b499ee72
jcisler <jedidahcherie@jedidahslersmbp.wireless.yale.internal>
2014-06-20 11:26:42 -0400

    I just added a line into the script to test how it works..

--- generate_data.sh
+++ generate_data.sh
@@ -1,6 +1,8 @@
 # use a for loop to create a text file called data1.txt with lines 1-10
 ##hint you can append to the end of a file with >>
 
+#adding for commit checks
+
     touch file1.txt
 for i in {0..10} ;
 do
